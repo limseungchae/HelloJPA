74544c462be4a11da46ba84cecbab56bd56e0e4c
limseungchae <tmdco12@naver.com>
2023-05-18 11:36:38 +0900

    hibernate HQL 실습

---
+++ src/main/java/notmap/Empinfo.java
@@ -0,0 +1,12 @@
+package notmap;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+public class Empinfo {
+    private String fname;
+    public  Integer sal;
+
+}
